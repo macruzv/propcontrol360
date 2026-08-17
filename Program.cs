421e898b87e583c6ddfa15af3bef7f457689a36d
mcruz2122 <92608231+mcruz2122@users.noreply.github.com>
2026-08-16 22:56:18 -0400

    Update server URL to port 8090 and update dist binaries

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using propcontrol360.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://127.0.0.1:8090");
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
